0c4e2e97d9fc46aab7837dc76e80fc133d1335b6
reneaas <reneaas@uio.no>
2020-09-03 18:30:50 +0200

    update

--- project1/main.py
+++ project1/main.py
@@ -1,21 +1,28 @@
 from Regression import *
 import numpy as np
+import sys
 
-path_to_datasets = "/Users/kasparagasvaer/Documents/FYS-STK4155/fys-stk4155/Project1/datasets/"
+#path_to_datasets = "/Users/kasparagasvaer/Documents/FYS-STK4155/fys-stk4155/Project1/datasets/"
+n = int(sys.argv[1])
+path_to_datasets = "./datasets/"
 
-filename = path_to_datasets + "frankefunction_dataset_N_1000_sigma_0.1.txt"
+filename = path_to_datasets + "frankefunction_dataset_N_" + str(n) + "_sigma_0.1.txt"
 
 deg = 5
 #Ender opp med å være 20 attributes, can't for the life of me generalisere
 #(hint: kombinatorikk, skal ikke ha med 0,0, men alle andre opp til 5 grad)
 #Vil egt sende inn typ 5, også fikse kombinatorikk magic i ReadData.
 
-n = 1000
+
 #vil vi vite punkter på forhånd eller finne dette ut i ReadData??
 
 solver = Regression(n)
-solver.ReadData(filename, deg)
-f_train, f_test, f_tilde, f_predict = solver.LinearRegression()
+solver.ReadData(filename, 5)
+solver.SplitData()
+solver.OLS()
+solver.Predict()
+solver.MSE()
+#f_train, f_test, f_tilde, f_predict = solver.LinearRegression()
 
 #Training
 #R2_train = solver.R2(f_train, f_tilde)
