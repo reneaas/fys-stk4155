eaa78f3bec49d1bb0d1e5ca3a3a07f402996d36e
kasparagaasvaer <kasparag@gmail.com>
2020-09-04 16:16:33 +0200

    Now plots saves the figures

--- project1/main.py
+++ project1/main.py
@@ -9,7 +9,7 @@ sigma = float(sys.argv[2]) #Standard deviation of noise from data
 path_to_datasets = "./datasets/" #relative path into subdirectory for datasets.
 filename = path_to_datasets + "frankefunction_dataset_N_" + str(n) + "_sigma_" + str(sigma) + ".txt"
 
-plot_OLS_MSE_R2(filename) #Plots the MSE and R2-score of the OLS regression
+plot_OLS_MSE_R2(filename, n, sigma) #Plots the MSE and R2-score of the OLS regression
 
 """
 polynomial_degree = 2 #Maximum degree of polynomial
--- project1/plot.py
+++ project1/plot.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from ols import *
 
 
-def plot_OLS_MSE_R2(filename):
+def plot_OLS_MSE_R2(filename,n,sigma):
+
     MSE_train = []
     MSE_test = []
     R2_train = []
@@ -29,13 +30,19 @@ def plot_OLS_MSE_R2(filename):
     R2_train = np.array(R2_train)
     R2_test = np.array(R2_test)
 
+    path = "./results/OLS/plots/"
+
+    plot_name = path + "MSE_N_" + str(n) + "sigma_" + str(sigma) + ".pdf"
+
     plt.plot(degrees, MSE_train, label = "Training data")
     plt.plot(degrees, MSE_test, label = "Test data")
     plt.title("MSE")
     plt.legend()
     plt.ylabel("Mean Square Error")
     plt.xlabel("Model Complexity")
-    plt.show()
+    plt.savefig(plot_name)
+
+    plot_name = path + "R2_N_" + str(n) + "sigma_" + str(sigma) + ".pdf"
 
     plt.plot(degrees, R2_train, label = "Training data")
     plt.plot(degrees, R2_test, label = "Test data")
@@ -43,4 +50,4 @@ def plot_OLS_MSE_R2(filename):
     plt.legend()
     plt.ylabel("R2 - Score")
     plt.xlabel("Model Complexity")
-    plt.show()
+    plt.savefig(plot_name)
