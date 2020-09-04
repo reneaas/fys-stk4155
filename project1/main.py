4bcd32cc4012dfbc684b1850e451b97945e18aa5
kasparagaasvaer <kasparag@gmail.com>
2020-09-04 16:07:09 +0200

    Moved plotting to seperate file

--- project1/main.py
+++ project1/main.py
@@ -1,5 +1,6 @@
 from regression import *
 from ols import *
+from plot import *
 import numpy as np
 import sys
 
@@ -8,7 +9,7 @@ sigma = float(sys.argv[2]) #Standard deviation of noise from data
 path_to_datasets = "./datasets/" #relative path into subdirectory for datasets.
 filename = path_to_datasets + "frankefunction_dataset_N_" + str(n) + "_sigma_" + str(sigma) + ".txt"
 
-
+plot_OLS_MSE_R2(filename) #Plots the MSE and R2-score of the OLS regression
 
 """
 polynomial_degree = 2 #Maximum degree of polynomial
@@ -19,45 +20,3 @@ solver.bootstrap(100)
 #solver.confidence_intervals(0.1)
 #solver.Bootstrap(3,5)
 """
-
-
-MSE_train = []
-MSE_test = []
-R2_train = []
-R2_test = []
-degrees = [1,2,3,4,5,7,8,9,10]
-solver = OLS()
-
-for deg in degrees:
-    solver.read_data(filename,deg)
-    solver.split_data()
-    solver.train()
-    solver.predict()
-    MTrain, MTest = solver.extract_MSE()
-    RTrain, RTest = solver.extract_R2()
-    MSE_train.append(MTrain)
-    MSE_test.append(MTest)
-    R2_train.append(RTrain)
-    R2_test.append(RTest)
-
-MSE_train = np.array(MSE_train)
-MSE_test = np.array(MSE_test)
-degrees = np.array(degrees)
-R2_train = np.array(R2_train)
-R2_test = np.array(R2_test)
-
-plt.plot(degrees, MSE_train, label = "Training data")
-plt.plot(degrees, MSE_test, label = "Test data")
-plt.title("MSE")
-plt.legend()
-plt.ylabel("Mean Square Error")
-plt.xlabel("Model Complexity")
-plt.show()
-
-plt.plot(degrees, R2_train, label = "Training data")
-plt.plot(degrees, R2_test, label = "Test data")
-plt.title("R2")
-plt.legend()
-plt.ylabel("R2 - Score")
-plt.xlabel("Model Complexity")
-plt.show()
---
+++ project1/plot.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ols import *
+
+
+def plot_OLS_MSE_R2(filename):
+    MSE_train = []
+    MSE_test = []
+    R2_train = []
+    R2_test = []
+    degrees = [1,2,3,4,5,7,8,9,10]
+    solver = OLS()
+
+    for deg in degrees:
+        solver.read_data(filename,deg)
+        solver.split_data()
+        solver.train()
+        solver.predict()
+        MTrain, MTest = solver.extract_MSE()
+        RTrain, RTest = solver.extract_R2()
+        MSE_train.append(MTrain)
+        MSE_test.append(MTest)
+        R2_train.append(RTrain)
+        R2_test.append(RTest)
+
+    MSE_train = np.array(MSE_train)
+    MSE_test = np.array(MSE_test)
+    degrees = np.array(degrees)
+    R2_train = np.array(R2_train)
+    R2_test = np.array(R2_test)
+
+    plt.plot(degrees, MSE_train, label = "Training data")
+    plt.plot(degrees, MSE_test, label = "Test data")
+    plt.title("MSE")
+    plt.legend()
+    plt.ylabel("Mean Square Error")
+    plt.xlabel("Model Complexity")
+    plt.show()
+
+    plt.plot(degrees, R2_train, label = "Training data")
+    plt.plot(degrees, R2_test, label = "Test data")
+    plt.title("R2")
+    plt.legend()
+    plt.ylabel("R2 - Score")
+    plt.xlabel("Model Complexity")
+    plt.show()
