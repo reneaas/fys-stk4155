be29e09e7a9704093a326065471fd87eab8933be
Maria Linea Horgen <marialinea1@hotmail.com>
2020-10-27 15:37:24 +0100

    update

--- project2/main.py
+++ project2/main.py
@@ -5,11 +5,11 @@ from functions import scale_data, mnist_data, test_model_mnist, design_matrix
 
 
 #Set up data:
-Ntrain = 10000
-Ntest = 1000
+Ntrain = 1000
+Ntest = 100
 X_train, Y_train, X_test, Y_test = mnist_data(Ntrain, Ntest)
 
-my_solver = FFNN(layers = 5, nodes = 100, X_data = X_train, y_data = Y_train, N_outputs = 10, hidden_activation = "ReLU", epochs = 10, Lambda=0.0001, gamma=0.1)
+my_solver = FFNN(layers = 5, nodes = 100, X_data = X_train, y_data = Y_train, N_outputs = 10, hidden_activation = "sigmoid", epochs = 10, Lambda=0.0001, gamma=0.9)
 
 start = time()
 my_solver.train()
