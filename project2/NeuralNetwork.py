cae099ec13752647b99512691fe8f151a9ee5d3f
reneaas <reneaas@uio.no>
2020-10-28 17:04:59 +0100

    Fixed neural network

--- project2/NeuralNetwork.py
+++ project2/NeuralNetwork.py
@@ -4,8 +4,8 @@ np.random.seed(1001)
 
 class FFNN():
 
-    def __init__(self, layers, nodes, X_data, y_data, N_outputs, epochs=10, batch_size=100, eta = 0.1, problem_type="classification", hidden_activation="sigmoid", Lambda=0, gamma=0):
-        self.layers = layers
+    def __init__(self, hidden_layers, nodes, X_data, y_data, N_outputs, epochs=10, batch_size=1, eta = 0.1, problem_type="classification", hidden_activation="sigmoid", Lambda=0, gamma=0):
+        self.layers = hidden_layers
         self.nodes = nodes
         self.X_data = X_data
         self.y_data = y_data
@@ -17,35 +17,42 @@ class FFNN():
         self.Lambda = Lambda              # regularization parameter
         self.gamma = gamma                # momentum parameter
 
-        #self.error_input = np.zeros(self.nodes)
-        self.error_hidden = np.zeros([self.layers,self.nodes])
-        self.error_output = np.zeros(self.N_outputs)
+        mean = 0; std = 0.0001
 
-        #self.grad_bias_input = np.zeros(self.nodes)
-        self.grad_bias_hidden = np.zeros([self.layers, self.nodes])
-        self.grad_bias_output = np.zeros(self.N_outputs)
 
-        self.grad_weights_input = np.zeros([self.nodes, self.features])
-        self.grad_weights_hidden = np.zeros([self.layers, self.nodes, self.nodes])
-        self.grad_weights_output = np.zeros([self.N_outputs,self.nodes])
 
-        self.activations = np.zeros([self.layers,self.nodes])
-        self.bias = np.random.normal(size=[self.layers, self.nodes])
-        self.bias_output = np.random.normal(size=self.N_outputs)
-
-        self.output = np.zeros(self.N_outputs)
-
-        mean = 0; std = 0.01 
+        self.activations_input = np.zeros(self.nodes)
+        self.activations_hidden = np.zeros([self.layers,self.nodes])
+        self.activations_output = np.zeros(self.N_outputs)
 
         self.weights_input = np.random.normal(mean, std, size=[self.nodes,self.features])
         self.weights_hidden = np.random.normal(mean, std, size=[self.layers, self.nodes, self.nodes])
         self.weights_output = np.random.normal(mean, std, size=[self.N_outputs,self.nodes])
 
+
+        self.bias_input = np.random.normal(mean, std, size=self.nodes)
+        self.bias_hidden = np.random.normal(mean, std, size=[self.layers, self.nodes])
+        self.bias_output = np.random.normal(mean, std, size=self.N_outputs)
+
+        self.grad_weights_input = np.zeros([self.nodes, self.features])
+        self.grad_weights_hidden = np.zeros([self.layers, self.nodes, self.nodes])
+        self.grad_weights_output = np.zeros([self.N_outputs,self.nodes])
+
+        self.grad_bias_input = np.zeros(self.nodes)
+        self.grad_bias_hidden = np.zeros([self.layers, self.nodes])
+        self.grad_bias_output = np.zeros(self.N_outputs)
+
+        self.error_input = np.zeros(self.nodes)
+        self.error_hidden = np.zeros([self.layers,self.nodes])
+        self.error_output = np.zeros(self.N_outputs)
+
+
+
         self.tmp_weights_input = np.zeros([self.nodes,self.features])
         self.tmp_weights_hidden = np.zeros([self.layers, self.nodes, self.nodes])
         self.tmp_weights_output = np.zeros([self.N_outputs,self.nodes])
-
-        self.tmp_bias = np.zeros([self.layers, self.nodes])
+        self.tmp_bias_input = np.zeros(self.nodes)
+        self.tmp_bias_hidden = np.zeros([self.layers, self.nodes])
         self.tmp_bias_output = np.zeros(self.N_outputs)
 
         if problem_type == "classification":
@@ -56,16 +63,25 @@ class FFNN():
 
 
         if hidden_activation == "sigmoid":
-            self.compute_hidden_act = lambda z: self.sigmoid(z)
+            self.compute_act = lambda z: self.sigmoid(z)
 
         if hidden_activation == "ReLU":
-            self.compute_hidden_act = lambda z: self.ReLU(z)
+            self.compute_act = lambda z: self.ReLU(z)
+            self.weights_input *= 1e-3
+            self.weights_hidden *= 1e-3
+            self.weights_output *= 1e-3
 
         if hidden_activation == "LeakyReLU":
-            self.compute_hidden_act = lambda z: self.LeakyReLU(z)
+            self.compute_act = lambda z: self.LeakyReLU(z)
+            self.weights_input *= 1e-3
+            self.weights_hidden *= 1e-3
+            self.weights_output *= 1e-3
 
         if hidden_activation == "ELU":
-            self.compute_hidden_act = lambda z: self.ELU(z)
+            self.compute_act = lambda z: self.ELU(z)
+            self.weights_input *= 1e-3
+            self.weights_hidden *= 1e-3
+            self.weights_output *= 1e-3
 
 
 
@@ -82,44 +98,60 @@ class FFNN():
                 self.y = self.y_data[indices]
                 for i in range(self.batch_size):
                     self.feed_forward(self.X[i])
+
                     self.backpropagate(self.X[i], self.y[i])
                 self.update_parameters()
+
+
         bar.finish()
 
 
     def feed_forward(self, x):
-        z = (self.weights_input @ x) + self.bias[0]
-        self.activations[0] = self.compute_hidden_act(z)
-        for l in range(1,self.layers):
-            z = self.weights_hidden[l] @ self.activations[l-1] + self.bias[l]
-            self.activations[l] = self.compute_hidden_act(z)
-        z = self.weights_output @ self.activations[-1] + self.bias_output
-        self.output = self.compute_output(z)
+        #Compute activations in input layer
+        z = (self.weights_input @ x) + self.bias_input
+        self.activations_input = self.compute_act(z)
+
+        #Compute activations in first hidden layer
+        z = self.weights_hidden[0] @ self.activations_input + self.bias_hidden[0]
+        self.activations_hidden[0] = self.compute_act(z)
+        #print("NN activation_hidden = ", self.activations_hidden)
+        #Compute activations in remaining hidden layers
+        for l in range(1, self.layers):
+            z = self.weights_hidden[l] @ self.activations_hidden[l-1] + self.bias_hidden[l]
+            self.activations_hidden[l] = self.compute_act(z)
+        z = self.weights_output @ self.activations_hidden[-1] + self.bias_output
+        self.activations_output = self.compute_output(z)
 
     def backpropagate(self, x, y):
         #Compute error at top layer
-        self.error_output = self.compute_error_output(self.output, y)
+        self.error_output = self.compute_error_output(self.activations_output, y)
 
         #update bias and weights
         self.grad_bias_output += self.error_output
-        self.grad_weights_output += np.outer(self.error_output, self.activations[-1]) + self.Lambda*self.weights_output
+        self.grad_weights_output += np.outer(self.error_output, self.activations_hidden[-1]) + self.Lambda*self.weights_output
 
-        s = self.activations[-1]* (1 - self.activations[-1])
+        s = self.activations_hidden[-1]*(1 - self.activations_hidden[-1])
         self.error_hidden[-1] = (self.weights_output.T @ self.error_output)*s
         self.grad_bias_hidden[-1] += self.error_hidden[-1]
-        self.grad_weights_hidden[-1] += np.outer(self.error_hidden[-1], self.activations[-2]) + self.Lambda*self.weights_hidden[-1]
+
+        if self.layers >= 2:
+            self.grad_weights_hidden[-1] += np.outer(self.error_hidden[-1], self.activations_hidden[-2]) + self.Lambda*self.weights_hidden[-1]
+            for l in range(self.layers-2, 0, -1):
+                s = self.activations_hidden[l] * (1 - self.activations_hidden[l])
+                self.error_hidden[l] = (self.weights_hidden[l+1].T @ self.error_hidden[l+1])*s
+                self.grad_bias_hidden[l] += self.error_hidden[l]
+                self.grad_weights_hidden[l] += np.outer(self.error_hidden[l], self.activations_hidden[l-1]) + self.Lambda*self.weights_hidden[l]
 
 
-        for l in range(self.layers-2, 0, -1):
-            s = self.activations[l] * (1 - self.activations[l])
-            self.error_hidden[l] = (self.weights_hidden[l+1].T @ self.error_hidden[l+1])*s
-            self.grad_bias_hidden[l] += self.error_hidden[l]
-            self.grad_weights_hidden[l] += np.outer(self.error_hidden[l], self.activations[l-1]) + self.Lambda*self.weights_hidden[l]
+            s = self.activations_hidden[0] * (1. - self.activations_hidden[0])
+            self.error_hidden[0] = (self.weights_hidden[1].T @ self.error_hidden[1])*s
+            self.grad_bias_hidden[0] += self.error_hidden[0]
+            self.grad_weights_hidden[0] += np.outer(self.error_hidden[0], self.activations_input) + self.Lambda*self.weights_hidden[0]
 
-        s = self.activations[0] * (1 - self.activations[0])
-        self.error_hidden[0] = (self.weights_hidden[1].T @ self.error_hidden[1])*s
-        self.grad_bias_hidden[0] += self.error_hidden[0]
-        self.grad_weights_input += np.outer(self.error_hidden[0], x) + self.Lambda*self.weights_input
+        s = self.activations_input * (1. - self.activations_input)
+        self.error_input = (self.weights_hidden[0].T @ self.error_hidden[0])*s
+        self.grad_bias_input += self.error_input
+        self.grad_weights_input += np.outer(self.error_input, x)
 
 
     def update_parameters(self):
@@ -134,8 +166,8 @@ class FFNN():
         self.weights_input -= self.tmp_weights_input
 
         for l in range(self.layers):
-            self.tmp_bias[l] = (self.grad_bias_hidden[l] + self.gamma*self.tmp_bias[l])
-            self.bias[l] -= self.tmp_bias[l]
+            self.tmp_bias_hidden[l] = (self.grad_bias_hidden[l] + self.gamma*self.tmp_bias_hidden[l])
+            self.bias_hidden[l] -= self.tmp_bias_hidden[l]
 
             self.tmp_weights_hidden[l] = (self.grad_weights_hidden[l] + self.gamma*self.tmp_weights_hidden[l])
             self.weights_hidden[l] -= self.tmp_weights_hidden[l]
@@ -154,7 +186,7 @@ class FFNN():
 
     def predict(self, x):
         self.feed_forward(x)
-        return self.output
+        return self.activations_output
 
     @staticmethod
     def softmax(z):
