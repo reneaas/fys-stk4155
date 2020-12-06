5da881b47a1a4b33bdef3a995f1e87705dbe9aab
reneaas <reneaas@fys.uio.no>
2020-12-06 16:35:23 +0100

    Added a more useful plot

--- project3/codes/neural_net/main_eigenvalue.py
+++ project3/codes/neural_net/main_eigenvalue.py
@@ -13,6 +13,7 @@ A = np.array([[3, 0, 4], [0, 2, 0], [4, 0, 3]])
 # Q = np.random.normal(0, 1, size=(mat_sz, mat_sz))
 # A = 0.5*(Q.T + Q)
 
+
 #Initialize the model
 input_sz = 1
 layers = [1, 100, 100, 3]
@@ -24,37 +25,59 @@ Nt = 100
 t_max = 1000
 x = np.random.normal(0, 1, size=mat_sz)
 t = np.linspace(0, t_max, Nt)
-epochs = 1000
-my_solver.fit(x = x, t = t, epochs = epochs)
+epochs = 100
+epoch_arr, eigvals, eigvecs = my_solver.fit(x = x, t = t, epochs = epochs)
+
+
+#Plot eigenvalue estimate as function of epochs
+fontsize = 12
+plt.plot(epoch_arr, eigvals)
+plt.xticks(fontsize=fontsize)
+plt.yticks(fontsize=fontsize)
+plt.xlabel("epochs", size=fontsize)
+plt.ylabel("eigenvalue estimate", size=fontsize)
+plt.legend(fontsize=fontsize)
+plt.show()
+
+#Plot eigenvector estimate as function of epochs
+for j in range(mat_sz):
+    plt.plot(epoch_arr, eigvecs[:,j], label=f"$x_{j}$")
+plt.xlabel("epochs", size=fontsize)
+plt.ylabel(r"$x_i(t)$", size=fontsize)
+plt.xticks(fontsize=fontsize)
+plt.yticks(fontsize=fontsize)
+plt.legend(fontsize=fontsize)
+plt.show()
 
 #Compute predictions. Computed eigenvalues converge as t --> inf.
 T = np.linspace(0, 1.1*t_max, 1001)
-eigenvalues = np.zeros_like(T)
-eigenvectors = np.zeros([len(T), mat_sz])
+eigvals = np.zeros_like(T)
+eigvecs = np.zeros([len(T), mat_sz])
 for i in range(len(T)):
     t = tf.constant(np.array([T[i]]).reshape(-1,1), dtype=tf.float32)
-    eigenvalue, eigenvector = my_solver.eig(x, t)
-    eigenvalues[i] = eigenvalue
+    eigval, eigvec = my_solver.eig(x, t)
+    eigvals[i] = eigval
     for j in range(mat_sz):
-        eigenvectors[i,j] = eigenvector.numpy()[j]
+        eigvecs[i,j] = eigvec.numpy()[j]
 
-true_eigenvalues, true_eigenvectors = np.linalg.eig(A)
-print("true eigenvalues = \n", true_eigenvalues)
-print("True eigenvectors = \n", true_eigenvectors)
-print("Estimated eigenvector =  \n", eigenvector.numpy())
-print("Estimated eigenvalue = ", eigenvalues[-1]) #Largest value for t.
+true_eigvals, true_eigvecs = np.linalg.eig(A)
+print("true eigenvalues = \n", true_eigvals)
+print("True eigenvectors = \n", true_eigvecs)
+print("Estimated eigenvector =  \n", eigvec.numpy())
+print("Estimated eigenvalue = ", eigvals[-1]) #Largest value for t.
 
 
-fontsize = 12
-plt.plot(T, eigenvalues)
+#Plot "eigenvalue prediction" as function of t for final model. Converges to true as t --> inf
+plt.plot(T, eigvals)
 plt.xticks(fontsize=fontsize)
 plt.yticks(fontsize=fontsize)
 plt.legend(fontsize=fontsize)
 plt.show()
 
 
+#plot "eigenvector prediction" as function of t for final model. Converges to true as t --> inf
 for j in range(mat_sz):
-    plt.plot(T, eigenvectors[:,j], label=f"$x_{j}$")
+    plt.plot(T, eigvecs[:,j], label=f"$x_{j}$")
 plt.xlabel(r"$t$", size=fontsize)
 plt.ylabel(r"$x_i(t)$", size=fontsize)
 plt.xticks(fontsize=fontsize)
--- project3/codes/neural_net/neural_eigensolver.py
+++ project3/codes/neural_net/neural_eigensolver.py
@@ -18,20 +18,28 @@ class NeuralEigenSolver(NeuralBase):
     def fit(self, x, t, epochs):
         #Set up meshgrid
         x = tf.convert_to_tensor(x, dtype=tf.float32)
+        x0 = x
         t = tf.convert_to_tensor(t, dtype=tf.float32)
         x, t = tf.meshgrid(x, t)
         x, t = tf.reshape(x, [-1, 1]), tf.reshape(t, [-1, 1])
         self.x, self.t = x, t
 
+        epoch_arr = np.linspace(1, epochs, epochs)
+        eigvals = np.zeros(epochs)
+        eigvecs = np.zeros([epochs, self.mat_sz])
         #fit the model
         bar = Bar("Epochs", max = epochs)
         for epoch in range(epochs):
             bar.next()
             loss, gradients = self.compute_gradients()
             self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-            #print(f"iteration: {epoch} ; loss: {loss.numpy()}")
+            t_max = tf.reshape(self.t[-1], [-1,1])
+            eigval, eigvec = self.eig(x0, t_max)
+            eigvals[epoch] = eigval.numpy()
+            eigvecs[epoch,:] = eigvec.numpy().T[:]
+
         bar.finish()
-        return None
+        return epoch_arr, eigvals, eigvecs
 
 
     @tf.function
