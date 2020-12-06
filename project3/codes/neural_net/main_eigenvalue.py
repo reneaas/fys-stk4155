8f85b8154b6c346251aa1770eb7b55aa567d3fbe
reneaas <reneaas@fys.uio.no>
2020-12-06 18:56:08 +0100

    Added version of eigenvalue plot.

--- project3/codes/neural_net/main_eigenvalue.py
+++ project3/codes/neural_net/main_eigenvalue.py
@@ -10,13 +10,14 @@ np.random.seed(seed)
 #Set up solver
 mat_sz = 3
 A = np.array([[3, 0, 4], [0, 2, 0], [4, 0, 3]])
+# mat_sz = 6
 # Q = np.random.normal(0, 1, size=(mat_sz, mat_sz))
 # A = 0.5*(Q.T + Q)
 
 
 #Initialize the model
 input_sz = 1
-layers = [1, 100, 100, 3]
+layers = [1, 100, 100, mat_sz]
 my_solver = NeuralEigenSolver(layers = layers, input_sz = input_sz, matrix = A)
 
 
@@ -28,14 +29,16 @@ t = np.linspace(0, t_max, Nt)
 epochs = 100
 epoch_arr, eigvals, eigvecs = my_solver.fit(x = x, t = t, epochs = epochs)
 
-
+true_eigvals, true_eigvecs = np.linalg.eig(A)
 #Plot eigenvalue estimate as function of epochs
 fontsize = 12
-plt.plot(epoch_arr, eigvals)
+plt.plot(epoch_arr, eigvals, label="estimate", color="r")
+plt.hlines(y = true_eigvals[:], xmin = 1, xmax = epochs, linestyles="dashed")
 plt.xticks(fontsize=fontsize)
 plt.yticks(fontsize=fontsize)
 plt.xlabel("epochs", size=fontsize)
 plt.ylabel("eigenvalue estimate", size=fontsize)
+plt.legend(fontsize=fontsize)
 plt.show()
 
 #Plot eigenvector estimate as function of epochs
@@ -66,14 +69,14 @@ print("Estimated eigenvector =  \n", eigvec.numpy())
 print("Estimated eigenvalue = ", eigvals[-1]) #Largest value for t.
 
 
+
 #Plot "eigenvalue prediction" as function of t for final model. Converges to true as t --> inf
-plt.plot(T, eigvals)
-plt.ylabel("eigenvalue estimate", size=fontsize)
-plt.xlabel(r"$t$", size=fontsize)
-plt.xticks(fontsize=fontsize)
-plt.yticks(fontsize=fontsize)
-# plt.legend(fontsize=fontsize)
-plt.show()
+# plt.plot(T, eigvals)
+# plt.ylabel("eigenvalue estimate", size=fontsize)
+# plt.xlabel(r"$t$", size=fontsize)
+# plt.xticks(fontsize=fontsize)
+# plt.yticks(fontsize=fontsize)
+# plt.show()
 
 
 #plot "eigenvector prediction" as function of t for final model. Converges to true as t --> inf
