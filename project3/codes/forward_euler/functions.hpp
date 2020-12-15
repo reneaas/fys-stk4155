5c9a29a161554eddd1d3664a848a942088cb2214
reneaas <reneaas@fys.uio.no>
2020-12-15 05:08:59 +0100

    update of very short readme for forward euler.

--- project3/codes/forward_euler/functions.hpp
+++ project3/codes/forward_euler/functions.hpp
@@ -8,13 +8,8 @@
 using namespace std;
 
 
-
-
-
 void initialize(double **v, double **x, double **t, int gridpoints, int timesteps, double dx, double dt);
-
 void explicit_scheme(double *v , double r, int gridpoints, int timesteps);
-
 void write_to_file(string outfilename, double *v, double *x, double *t, int gridpoints, int timesteps);
 
 
